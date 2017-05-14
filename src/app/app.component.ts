09aea664da7d681a5998b4607de8f7c0f9a8a2d4
AVAndriets <avandriets@gmail.com>
2017-05-14 22:55:09 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+declare var UIkit: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  user: Observable<firebase.User>;
+
+  constructor(){
+  }
+
+  login() {
+  }
+
+  logout() {
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB9LhiF3IUAIhKtbxzP2dKW8FtVQq7BU70",
+    authDomain: "luckyclub-897a7.firebaseapp.com",
+    databaseURL: "https://luckyclub-897a7.firebaseio.com",
+    projectId: "luckyclub-897a7",
+    storageBucket: "luckyclub-897a7.appspot.com",
+    messagingSenderId: "424759080154"
+  }
 };
