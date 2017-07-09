627a374130dcc33709987d971a1da128980d7cb0
AVAndriets <avandriets@gmail.com>
2017-07-09 13:59:23 +0300

    add scss files to project

--- src/app/views/lots/lots.component.ts
+++ src/app/views/lots/lots.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lucky-lots',
   templateUrl: './lots.component.html',
-  styleUrls: ['./lots.component.css']
+  styleUrls: ['./lots.component.scss']
 })
 export class LotsComponent implements OnInit {
 
