5a26c9904ef5558503c8f00ee3abb0366fe2e42c
OSA <osandriiets@gmail.com>
2017-07-16 23:21:59 +0300

    change lot component

--- src/app/views/lots/new-lot/new-lot.component.ts
+++ src/app/views/lots/new-lot/new-lot.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'lucky-new-lot',
@@ -10,9 +10,20 @@ export class NewLotComponent implements OnInit {
   lotCreateFG: FormGroup = null;
   file:any = null;
 
-  constructor() { }
+  constructor(  ) {
+
+     this.lotCreateFG = new FormGroup({
+        name: new FormControl(null, Validators.required),
+        description: new FormControl(null, Validators.required),
+        category_id: new FormControl(null, Validators.required),
+        price:  new FormControl(null, Validators.required)
+    })
+  }
 
   ngOnInit() {
+  }
+  onSubmit(){
+
   }
 
 }
