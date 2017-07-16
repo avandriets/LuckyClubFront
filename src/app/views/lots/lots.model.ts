71cdc8a6ebf99cd264f47d4d636d6c3d469dc4ce
OSA <osandriiets@gmail.com>
2017-07-16 22:52:48 +0300

    change lot model

--- src/app/views/lots/lots.model.ts
+++ src/app/views/lots/lots.model.ts
@@ -2,26 +2,21 @@ export class Lot {
   id: number;
   name: string;
   description: string;
-  picture_file: string;
-  picture_url: string;
-  parent_id: number;
+  category_id: number;
+  published: boolean;
+  finished: boolean;
+  deleted: boolean;
+  winner_id: number;
+  count_participants: number;
+  price:  number;
+  owner_id: number;
   created_at: string;
   updated_at: string;
+  pictures: string;
+  owner_profile: number;
 
-  constructor(parameter?: {id: number, name: string, description: string, picture_file: string, picture_url: string, parent_id: number, created_at: string, updated_at: string}) {
+  constructor(parameter?: { id: number, name: string, description: string,  category_id: number, published: boolean, finished: boolean, deleted: boolean, winner_id: number, count_participants: number, price:  number,  owner_id: number, created_at: string, updated_at: string, pictures: string, owner_profile: number
+}) {
     Object.assign(this, parameter);
   }
-}
-
-export class LotsCollection{
-
-  lots: Lot[] = [];
-
-  constructor(lots?: Lot[]) {
-    if(lots){
-      this.lots = lots;
-    }
-  }
-
-
 }
