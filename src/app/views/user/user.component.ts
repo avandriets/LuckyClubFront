750131272f5bf3897e4f6ac022b9bf203ec4d8e9
OSA <osandriiets@gmail.com>
2017-07-24 21:22:24 +0300

    lots of changes user

--- src/app/views/user/user.component.ts
+++ src/app/views/user/user.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {Users} from "../../auth/auth.model";
+import {Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import {Users, LoginStatusEnum} from "../../auth/auth.model";
 import {AuthService} from "../../auth/auth.service";
+import {LotsServiseService} from "../lots/lots-servise.service";
+import {Lot} from "../lots/lots.model";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'lucky-user',
@@ -11,11 +15,66 @@ export class UserComponent implements OnInit {
 
   currentUser: Users = null;
 
-    constructor(private authService: AuthService) {
+  id: number = 0;
+  recommend_lots: Lot[] = [];
+  favorite_lots: Lot[] = [];
+
+  private authStatusSubscription: Subscription;
+  private subscription: Subscription;
+
+    constructor(
+      private lotSrv: LotsServiseService,
+      private router: Router,
+      private route: ActivatedRoute,
+      private authSrv: AuthService,
+      private changeDetection: ChangeDetectorRef) {
   }
 
   ngOnInit() {
-    this.currentUser = this.authService.getCurrentUser();
+    this.currentUser = this.authSrv.getCurrentUser();
+
+
+    this.subscription = this.lotSrv.invokeEvent
+      .subscribe(
+        () => {
+          this.getData();
+        }
+      );
+
+    this.authStatusSubscription = this.authSrv.invokeEvent.subscribe(
+      (statusValue: LoginStatusEnum) => {
+        this.getData();
+      }
+    );
+
+    this.getData();
+
   }
 
+  getData() {
+
+    this.lotSrv.getRecommend().subscribe(
+      (data: Lot[]) => {
+        this.recommend_lots = data;
+      }
+    );
+
+    this.lotSrv.getFavorites().subscribe(
+      (data: Lot[]) => {
+        this.favorite_lots = data;
+        this.changeDetection.detectChanges();
+      },
+      (error) => {
+        this.favorite_lots = [];
+      }
+    );
+  }
+
+  onMoreRecommend() {
+    console.log('onMoreRecommend');
+  }
+
+  onMoreFavorite(){
+    console.log('onMoreFavorite');
+  }
 }
